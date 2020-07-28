e8eb7e8e09458e2d7d3ef86797b7469f7970a05d
swsoyee <swsoyee@gmail.com>
2020-07-28 12:47:17 +0900

    chore: format code

--- 175-Combine-Two-Tables.sql
+++ 175-Combine-Two-Tables.sql
@@ -1,8 +1,9 @@
- -- 175 Combine Two Tables
-SELECT  FirstName 
-       ,LastName 
-       ,City 
-       ,State
-FROM Person
-LEFT JOIN Address
-ON Person.PersonId = Address.PersonId
+-- 175 Combine Two Tables
+SELECT
+       FirstName,
+       LastName,
+       City,
+       State
+FROM
+       Person
+       LEFT JOIN Address ON Person.PersonId = Address.PersonId
--- 176-Second-Highest-Salary.sql
+++ 176-Second-Highest-Salary.sql
@@ -2,17 +2,35 @@
 /*
  Method 1
  */
-SELECT  MAX(Salary) AS SecondHighestSalary
-FROM Employee
-WHERE Salary < ( 
-SELECT  MAX(Salary)
-FROM Employee)
-
+(
+    SELECT
+        MAX(Salary) AS SecondHighestSalary
+    FROM
+        Employee
+    WHERE
+        Salary < (
+            SELECT
+                MAX(Salary)
+            FROM
+                Employee
+        )
+)
 /*
  Method 2, a better solution
-*/
-SELECT  IFNULL( (
-SELECT  distinct (salary)
-FROM employee
-ORDER BY salary desc
-LIMIT 1 offset 1), null) AS SecondHighestSalary
+ */
+(
+    SELECT
+        IFNULL(
+            (
+                SELECT
+                    distinct (salary)
+                FROM
+                    employee
+                ORDER BY
+                    salary desc
+                LIMIT
+                    1 offset 1
+            ),
+            null
+        ) AS SecondHighestSalary
+)
